8e86fb0fe49e69bc79d7326261466ee5eac5ceba
Michael Corcoran <michael.f.corcoran@nasa.gov>
2019-09-27 08:15:31 -0400

    mfc - updated parameter prompting to follow rules given in
    https://heasarc.gsfc.nasa.gov/docs/software/lheasoft/headas/pil/node12.html

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import sys
 def _ask_for_param(p_name, p_dict):
 
     if 'prompt' in p_dict[p_name]:
-        query_msg = 'No value found for {0}.\n{1}'.format(p_name, p_dict[p_name]['prompt']+'> ')
+        query_msg = '{1}'.format(p_name, p_dict[p_name]['prompt']+'> ')
     else:
         query_msg = 'No value found for {0}.\nPlease enter: '.format(p_name+' > ')
     usr_inp = ''
@@ -18,3 +18,20 @@ def _ask_for_param(p_name, p_dict):
         except EOFError:
             sys.exit('\nKeyboard interrupt received, program stopping.')
     return usr_inp
+
+def _check_query_param(p_name, p_dict):
+    """
+    checks whether a user should be queried for a parameter value if parameter not specified
+    see https://heasarc.gsfc.nasa.gov/docs/software/lheasoft/headas/pil/node12.html
+    :param p_name: parameter name
+    :param p_dict: parameter dictionary for function
+    :return: True if user needs to be queried for parameter value, False if not
+    """
+    ans= False
+    if 'a' in p_dict[p_name]['mode']:
+        if 'q' in p_dict['mode']['default']:
+            ans = True
+    if 'q' in p_dict[p_name]['mode']:
+        ans = True
+    return ans
+
