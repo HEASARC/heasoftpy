e15e98fd615803ca0650a382b2fbf329721098fb
Matt Elliott <matthew.h.elliott@nasa.gov>
2021-02-09 17:40:35 -0500

    Added code to check parameters before the HEASoft tool is called.

--- utils/__init__.py
+++ utils/__init__.py
@@ -105,6 +105,63 @@ def check_query_param(p_name, p_dict):
         ans = True
     return ans
 
+def is_param_ok(param, par_dict):
+    """
+    Returns True when the parameter passed in via param is verified to comply with
+    the rules in par_dict. False is returned otherwise.
+
+    Function parameters:
+    param - a 2 item tuple containing indicating the parameter's name and value.
+    par_dict - the parameter rules dictionary against which param should be checked
+    """
+    param_ok = False
+#    print('checking {}'.format(param[0]))
+    if param[0] in par_dict:
+#        print('Maybe')
+#        print(par_dict[param[0]])
+        param_type = par_dict[param[0]]['type']
+        if param_type == 's':
+            # Is the following redundant? Everything will be a string when it "arrives"  ...
+#            print('Testing for string')
+            if isinstance(param[1], str):
+#                print('{} is a string'.format(param[1]))
+                param_ok = True
+        elif param_type == 'i':
+#            print('Testing {} as int.'.format(param[1]))
+            try:
+                _ = int(param[1])
+                param_ok = True
+#                print('{} is an int'.format(param[1]))
+            except ValueError:
+                #pass
+                print('ValueError encountered converting "{}" to int'.format(param[1]))
+        elif param_type == 'b':
+            if isinstance(param[1], str):
+                param_lower = param[1].lower()
+                if param_lower in ['y', 'yes', 'n', 'no']:
+                    param_ok = True
+#            print('Testing for boolean')
+            # To do: Figure what what is acceptable here. They seem to want "y", "n", "t", "f",
+            # "1", "0", etc. to be acceptable ...
+        elif param_type == 'r':
+#            print('Testing for real')
+            try:
+                _ = float(param[1])
+                param_ok = True
+#                print('{} is an float'.format(param[1]))
+            except ValueError:
+                #pass
+                print('ValueError encountered converting "{}" to float'.format(param[1]))
+        elif param_type.find('f') != -1:
+#            print('Testing for filename')
+            ### For now, checking for str. To do: find out what needs to be do for a "file type"
+            if isinstance(param[1], str):
+#                print('{} is a string'.format(param[1]))
+                param_ok = True
+    else:
+        print('Error! Invalid parameter type {}'.format(param[0]))
+    return param_ok
+
 def read_version(module_dir):
     """
     Returns a string containing the version, as read from the version file.
