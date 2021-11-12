e7998c3f529b79c61fb297a8150d996d0b1481ec
Matt Elliott <matthew.h.elliott@nasa.gov>
2021-11-12 17:30:32 -0500

    Fix problems with wrappers for programs that accept a single, keyword-less argument. Update tests.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -169,16 +169,11 @@ File: test_rate.fit
 
 """
 
-#    @unittest.skip('skipping test_A')
-#    def test_A(self):
-#        print('in test_A: ', re.match(FTVERIFY_EXP_OUT, self.ftv_out))
-#        self.assertRegex(self.ftv_out, FTVERIFY_EXP_OUT)
-
     def tearDown(self):
         """ Currently a placeholder """
         #pass
 
-#    @unittest.skip('skipping fhelp')
+#    @unittest.skip('skipping test_fhelp')
     def test_fhelp(self):
         """
         Test the fthelp program by retrieving the help for ftlist.
@@ -191,10 +186,10 @@ File: test_rate.fit
         self.assertTrue(test_out[0].strip() == 'NAME' and test_out[1].strip() == '' and
                         test_out[2].strip() == 'ftlist - List the contents of the input file.')
 
-#    @unittest.skip('skipping fhelp_pos_arg')
+#    @unittest.skip('skipping test_fhelp_pos_arg')
     def test_fhelp_pos_arg(self):
         """
-        Test the fthelp program by retrieving the help for ftlist.
+        Test the fhelp program by retrieving the help for ftlist.
         """
         help_out = heasoftpy.fhelp('ftlist')
         #string_out = byte_out.decode()
@@ -204,6 +199,20 @@ File: test_rate.fit
         self.assertTrue(test_out[0].strip() == 'NAME' and test_out[1].strip() == '' and
                         test_out[2].strip() == 'ftlist - List the contents of the input file.')
 
+#    @unittest.skip('skipping test_fthelp_pos_arg')
+    def test_fthelp_pos_arg(self):
+        """
+        Test the fthelp program by retrieving the help for ftlist.
+        """
+        help_out = heasoftpy.fthelp('ftlist')
+        #string_out = byte_out.decode()
+        test_out = []
+        for test_line in help_out.stdout.split('\n'):
+            test_out.append(test_line)
+        self.assertTrue(test_out[0].strip() == 'NAME' and test_out[1].strip() == '' and
+                        test_out[2].strip() == 'ftlist - List the contents of the input file.')
+
+#    @unittest.skip('skipping test_ftcopy')
     def test_ftcopy(self):
         """
         Test the ftcopy program by copying the test file to a new file
@@ -220,7 +229,7 @@ File: test_rate.fit
         #print(test_out)
         self.assertTrue(os.path.exists(copy_name))
 
-#    @unittest.skip('skipping fthelp')
+#    @unittest.skip('skipping test_fthelp')
     def test_fthelp(self):
         """
         Test the fthelp program by retrieving the help for ftlist.
@@ -233,19 +242,19 @@ File: test_rate.fit
         self.assertTrue(test_out[0].strip() == 'NAME' and test_out[1].strip() == '' and
                         test_out[2].strip() == 'ftlist - List the contents of the input file.')
 
-#    @unittest.skip('skipping ftlist')
+#    @unittest.skip('skipping test_ftlist')
     def test_ftlist(self):
         """ Test the ftlist program """
         test_out = heasoftpy.ftlist(infile=self.test_filepath, option='H').stdout
         self.assertEqual(test_out, FTLIST_EXP_OUT)
 
-#    @unittest.skip('skipping ftlist_dot_output')
+#    @unittest.skip('skipping test_ftlist_dot_output')
     def test_ftlist_dot_output(self):
         """ Test the ftlist program """
         test_out = heasoftpy.ftlist(infile=self.test_filepath, option='H').output
         self.assertEqual(test_out, FTLIST_EXP_OUT.split('\n'))
 
-    #@unittest.skip('skipping ftstat')
+    #@unittest.skip('skipping test_ftstat')
     def test_ftstat(self):
         """ Test the ftlist program """
         test_out = heasoftpy.ftstat(infile=self.test_filepath, min=0, max=0,
@@ -257,28 +266,22 @@ File: test_rate.fit
         self.assertRegex(test_out, FTSTAT_EXP_PATTERN)
 
     # Need to figure out why the assertRegex fails
-    #@unittest.skip('Skipping ftverify')
+    #@unittest.skip('Skipping test_ftverify')
     def test_ftverify(self):
         """ Tests the ftverify program """
-#        print('\nEntering test_ftverify\n')
         # It seems we need to give the specific and full path here
         test_result = heasoftpy.ftverify(infile=self.test_filepath,
                                          numerrs=0, numwrns=0,
                                          stderr=True)
-#        test_result = heasoftpy.ftverify(self.test_filepath, stderr=True)
-                                         
         test_out = test_result.stdout
         test_err = test_result.stderr
         if isinstance(test_out, bytes):
             test_out = test_out.decode()
-#        print('\ntest_out\n-----\n{}\n-----\n'.format(test_out))
         if isinstance(test_err, bytes):
             test_err = test_err.decode()
         self.assertRegex(test_out, FTVERIFY_EXP_PATTERN)
-#        print('\nExitng test_ftverify\n')
 
-    # Need to figure out why the assertRegex fails
-    #@unittest.skip('Skipping ftverify_single_arg')
+#    @unittest.skip('Skipping test_ftverify_single_arg')
     def test_ftverify_single_arg(self):
         """ Tests the ftverify program using only one (positional) argument"""
         test_result = heasoftpy.ftverify(self.test_filepath, stderr=True)
