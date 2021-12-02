21c3eb438932df1b7de106f8f607b5ffba34c3fa
Abdu Zoghbi <astrozoghbi@gmail.com>
2021-12-02 09:10:45 -0500

    added template_lib.py which was missed from the last commit

---
+++ heasoftpy/contrib/template/template_lib.py
@@ -0,0 +1,69 @@
+
+
+import sys
+
+from ...core import HSPTask, HSPResult, HSPLogger
+from ... import fcn, utils
+
+
+class TemplateTask(HSPTask):
+    """Sample python-based task"""
+    
+    
+    def exec_task(self):
+        
+        # put the parameters in to a list of par=value
+        params = self.params
+        
+        # logger
+        logger = self.logger
+        
+        # return code: 0 if task runs sucessful; set to 0 at the end
+        returncode = 1
+        
+        
+        ## ----------------- ##
+        ##  start code here  ##
+        ## ----------------- ##
+        msg = f"""\nResetting the foo parameter from {params['foo']} to {params['bar']}.\n"""        
+        logger.info(msg)
+        
+        params['foo'] = f"{params['bar']}" #  stringify the int
+        msg = f"Now foo = {params['foo']}."
+        logger.info(msg)
+        
+        params['bar'] = _some_addition_function(params['bar'])
+        msg = f" and bar = {params['bar']}.\n"
+        logger.info(msg)
+        
+        logger.error('No error, but checking the logger works\n')
+        returncode = 0
+        ## ----------------- ##
+        ##  end code  ##
+        ## ----------------- ##
+
+        outMsg, errMsg = logger.get_output()
+        return HSPResult(returncode, outMsg, errMsg, params)
+    
+    
+    def task_docs(self):
+        """Leave like this to use the docstring in the task method below 
+        This is the help text to be printed when passing -h to the command 
+        line executable of the task
+        """
+        return template.__doc__
+
+
+def _some_addition_function(inval):
+    return inval+1
+
+
+def template(args=None, **kwargs):
+    """
+    Write your task help text here.
+    
+    """
+    template_task = TemplateTask('template')
+    result = template_task(args, **kwargs)
+    return result
+
