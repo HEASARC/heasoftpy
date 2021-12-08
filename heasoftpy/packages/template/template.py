b06ee0cfdf30fba42c5cbbb5f38e8736419a5489
Abdu Zoghbi <astrozoghbi@gmail.com>
2021-12-08 17:20:31 -0500

    updated template.py to use heasofpy.TemplateTask instead of hsp.package.TemplateTask

--- heasoftpy/packages/template/template.py
+++ heasoftpy/packages/template/template.py
@@ -3,12 +3,11 @@
 
 import sys
 import heasoftpy as hsp
-import heasoftpy.packages as hspP
 
 
 if __name__ == '__main__':
     
-    task = hspP.TemplateTask(name='template')
+    task = hsp.TemplateTask(name='template')
     cmd_args = hsp.utils.process_cmdLine(task)
     result = task(**cmd_args)
     sys.exit(result.returncode)
