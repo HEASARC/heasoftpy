3f3254cefb3e18ec803bbbdffa98b438767ce079
Abdu Zoghbi <a.zoghbi@nasa.gov>
2022-11-01 21:10:12 -0400

    moved a unit test to different location

--- tests/test_params.py
+++ tests/test_params.py
@@ -300,7 +300,19 @@ class TestWritePFile(unittest.TestCase):
         # --- #
         os.environ['PFILES'] = pfiles
         os.remove(f'{taskname}.par')
- 
+        
+        
+
+class TestParamExtra(unittest.TestCase):
+    """Some additional tests for handling parameters"""
+    
+    # a parameter that expects a comma-separated list, given inside quotes.
+    # heasoft tasks don't like that
+    def test__comma_list_inside_quotes(self):
+        task = heasoftpy.HSPTask('fdump')
+        res1 = task(infile='tests/test.fits', outfile='STDOUT', columns='"TIME,RATE"', rows='-', more='no', prhead='no')
+        self.assertEqual(res1.returncode, 0)
+
         
 if __name__ == '__main__':
     unittest.main()
