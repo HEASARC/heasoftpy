67d664a39c2e311313194962c5ef756751677fd6
Abdu Zoghbi <a.zoghbi@nasa.gov>
2025-02-19 20:20:52 -0500

    add tests for find_module_name

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,9 @@
 from .context import heasoftpy
 
 import os
+import sys
 import unittest
+from unittest.mock import patch
 
 
 class TestUtils(unittest.TestCase):
@@ -63,6 +65,30 @@ class TestUtils(unittest.TestCase):
             self.assertTrue(pDir in os.environ['PFILES'])
 
         self.assertFalse(pDir in os.environ['PFILES'])
+
+    def test_find_module_name__unknown(self):
+        with self.assertRaises(ValueError):
+            heasoftpy.utils.find_module_name('unknown')
+
+    def test_find_module_name(self):
+        self.assertTrue(
+            'ftools' == heasoftpy.utils.find_module_name('fdump')
+        )
+        self.assertTrue(
+            'heatools' == heasoftpy.utils.find_module_name('ftlist')
+        )
+        self.assertTrue(
+            'attitude' == heasoftpy.utils.find_module_name('attconvert')
+        )
+
+    def test_find_module_name__w_hsp_nokey(self):
+        with patch('heasoftpy._modules.mapper', {}):
+            self.test_find_module_name()
+
+    def test_find_module_name__w_hsp_importError(self):
+        with patch.dict(sys.modules, {"heasoftpy._modules": None}):
+            self.test_find_module_name()
+
 
 
 # def test_pfiles_list():
